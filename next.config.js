f8d38b7ffd33ca23369f5b2b054d16849357832e
tevinthuku <tevinthuku@gmail.com>
2018-11-04 14:13:09 +0300

    Back to default config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 // next.config.js
-module.exports = {
+const withWorkers = require("@zeit/next-workers");
+module.exports = withWorkers({
   webpack(config, options) {
-    config.module.rules.push({
-      test: /\.worker\.js$/,
-      loader: "worker-loader",
-      // options: { inline: true }, // also works
-      options: {
-        name: "static/[hash].worker.js",
-        publicPath: "/_next/"
-      }
-    });
     return config;
   }
-};
+});
