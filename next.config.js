f86e6a11ebcdce1761a2dd515baf6b95369c79c3
tevinthuku <tevinthuku@gmail.com>
2018-11-20 22:11:22 +0300

    Found solution

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const withWorkers = require("@zeit/next-workers");
 module.exports = withWorkers({
   webpack(config, options) {
+    // add this to make the web workers work in next js version 7
+    config.output.globalObject = `(typeof self !== 'undefined' ? self : this)`;
     return config;
   }
 });
